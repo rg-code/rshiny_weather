46f6e7998d16ec4e9303686f25d100c6f90f423c
Rohit Gupta <rgupta6@ucmerced.edu>
2019-02-20 16:58:41 -0800

    server_shiny

---
+++ server.R
@@ -0,0 +1,23 @@
+ibrary(shiny)
+
+# Preprocessing and summarizing data
+library(dplyr)
+
+# Visualization development
+library(ggplot2)
+
+# For text graphical objects (to add text annotation)
+library(grid)
+
+source("viz_temps.R")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  output$DublinTemps <- renderPlot({
+    viztemps(input$start_year,
+             input$hide_hilows,
+             input$hide_avgs
+    )
+  })
+})
